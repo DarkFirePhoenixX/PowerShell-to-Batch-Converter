545643c73cb065b711f486a9a77afa44b368a355
Stanislav Slavkov <s.slavkov20@gmail.com>
2023-03-28 12:35:39 +0300

    Initial commit

---
+++ Powershell-to-Bat.ps1
@@ -0,0 +1,30 @@
+﻿function Convert-PowerShellToBat
+{
+    param
+    (
+        [Parameter(Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [string]
+        [Alias("FullName")]
+        $Path
+    )
+ 
+    process
+    {
+        $encoded = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes((Get-Content -Path $Path -Raw -Encoding UTF8)))
+        $newPath = [Io.Path]::ChangeExtension($Path, ".bat")
+        "@echo off`npowershell.exe -NoExit -encodedCommand $encoded" | Set-Content -Path $newPath -Encoding Ascii
+    }
+}
+
+Add-Type -AssemblyName System.Windows.Forms
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
+    InitialDirectory = [Environment]::GetFolderPath('Desktop') 
+    Filter = 'Powershell-Scripts (*.ps1)|*.ps1'
+}
+
+if('Ok' -eq $FileBrowser.ShowDialog()){
+	Get-ChildItem -Path $FileBrowser.filename -Filter *.ps1 |
+ Convert-PowerShellToBat; exit
+}else{
+	exit
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# PowerShell-To-Batch-Converter
+ Small script to convert powershell to batch files allowing you to execute your scripts without enabling "running scripts" on Windows.
